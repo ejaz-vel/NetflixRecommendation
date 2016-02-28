b20d8f0c3a7262e2c2b3dc2d57f31a843b3438dd
Ejaz Veljee <eveljee@andrew.cmu.edu>
2016-02-28 11:45:06 -0500

    Neighbourhood based models

--- Recommend.py
+++ Recommend.py
@@ -1,15 +1,36 @@
 from scipy.sparse import csr_matrix
 import math
+import time
+import os
 
-def normalizeMatrixRow(data, lastUpdated, currentIndex, sumOfSquares):
-	normFactor = math.sqrt(sumOfSquares)
-	data[lastUpdated:currentIndex] = [x/normFactor for x in data[lastUpdated:currentIndex]]
+imputationConstant = 3
+globalUserDict = {}
+globalMovieDict = {}
 
-def getUserVectors():
+def computeCosineSimilarity(vector1, vector2):
+	sim = vector1.dot(vector2.T)
+	if sim.nnz == 0:
+		return 0.0
+	else:
+		norm1 = math.sqrt(vector1.multiply(vector1).sum())
+		norm2 = math.sqrt(vector2.multiply(vector2).sum())
+		return sim.data[0]/(norm1 * norm2)
+
+def computeDotProduct(vector1, vector2):
+	sim = vector1.dot(vector2.T)
+	if sim.nnz == 0:
+		return 0.0
+	else:
+		return float(sim.data[0])
+
+def standardizeMatrixRow(data, lastUpdated, current):
+	mean = (sum(data[lastUpdated:current]) + 0.0) / len(data[lastUpdated:current])
+	data[lastUpdated:current] = [(x - mean) for x in data[lastUpdated:current]]
+
+def getUserVectors(standardizationRequired = False):
 	row = []
 	col = []
 	data = []
-	imputationConstant = 3
 	
 	numRows = 0
 	numMovies = 0
@@ -19,38 +40,221 @@ def getUserVectors():
 	for line in iter(f):
 		userData = line.split()
 		userID = int(userData[0])
-		
-		sumOfSquares = 0
 		lastUpdatedIndex = currentIndex
 		for rating in userData[1:]:
 			movieID = int(rating.split(":")[0])
 			rating = int(rating.split(":")[1]) - imputationConstant
-			sumOfSquares += math.pow(rating, 2)
-			
+			if(rating == 0):
+				continue
 			if numMovies < movieID:
 				numMovies = movieID
-
 			row.append(userID)
 			col.append(movieID)
 			data.append(rating)
 			currentIndex += 1
-		
-		if sumOfSquares != 0:
-			normalizeMatrixRow(data, lastUpdatedIndex, currentIndex, sumOfSquares)
-		
+		if standardizationRequired is True and lastUpdatedIndex != currentIndex:
+			standardizeMatrixRow(data, lastUpdatedIndex, currentIndex)
 		if numRows < userID:
 			numRows = userID
+	f.close()
 		
 	numRows += 1
 	numMovies += 1
 	userVectors = csr_matrix((data, (row, col)), shape=(numRows, numMovies))
 	
 	return numRows, numMovies, userVectors
-
-def findKNearestNeighbours(k, userID, userVectors, numUsers):
-	currentUser = userVectors.getrow(userID)
-	for i in range(numUsers):
-		if i == userID:
-			continue
 		
-		sampleUser = userVectors.getrow(i)
+def computeItemAssociationsUsingDotProduct(userVectors):
+	itemAssociations = userVectors.T * userVectors
+	return itemAssociations
+
+def computeItemAssociationsUsingCosineSimilarity(userVectors, numMovies):
+	row = []
+	col = []
+	data = []
+	
+	if os.path.exists("itemAssociations.txt"):
+		f = open("itemAssociations.txt")
+		for line in iter(f):
+			element = line.split()
+			row.append(str(element[0]))
+			col.append(str(element[1]))
+			data.append(str(element[1]))
+	else:
+		for movieIDRow in range(numMovies):
+			if userVectors.getcol(movieIDRow).nnz == 0:
+				continue
+			print "Filling Row: " + str(movieIDRow)
+			for movieIDCol in range(numMovies):
+				if userVectors.getcol(movieIDCol).nnz == 0:
+					continue
+				row.append(movieIDRow)
+				col.append(movieIDCol)
+				data.append(computeCosineSimilarity(userVectors.getcol(movieIDRow).T, userVectors.getcol(movieIDCol).T))
+			
+		associationsFile = open("itemAssociations.txt", "w")
+		for index in range(len(row)):
+			associationsFile.write(str(row[index]) + " " + str(col[index]) + " " + str(data[index]) + "\n")
+		associationsFile.close()
+		
+	itemAssociations = csr_matrix((data, (row, col)), shape=(numMovies, numMovies))
+	return itemAssociations
+
+def getTopK(neighbourList, k):
+	kList = []
+	for i in range(k):
+		if len(neighbourList) == 0:
+			break
+
+		pos = neighbourList.index(max(neighbourList, key=lambda x: x[1]))
+		kList.append(neighbourList[pos])
+		neighbourList.pop(pos)
+	return kList
+
+def findKSimilarUsers(k, userID, userVectors, numUsers, useDotProduct):
+	if not globalUserDict.has_key(userID):
+		neighbourList = []
+		currentUser = userVectors.getrow(userID)
+		for i in range(numUsers):
+			sampleUser = userVectors.getrow(i)
+			if i == userID or sampleUser.nnz == 0:
+				continue
+			
+			if useDotProduct is True:
+				similarity = computeDotProduct(currentUser, sampleUser)
+			else:
+				similarity = computeCosineSimilarity(currentUser, sampleUser)
+			neighbourList.append((i, similarity))
+		globalUserDict[userID] = getTopK(neighbourList, k)
+	return globalUserDict[userID]
+
+def findKSimilarMovies(k, movieID, itemAssociations, numMovies):
+	if not globalMovieDict.has_key(movieID):
+		neighbourList = []
+		currentMovie = itemAssociations.getrow(movieID)
+		for i in range(numMovies):
+			if i == movieID:
+				continue
+			
+			if currentMovie.getcol(i).nnz != 0:
+				neighbourList.append((i, currentMovie.getcol(i).data[0]))
+		globalMovieDict[movieID] = getTopK(neighbourList, k)
+	return globalMovieDict[movieID]
+
+def computePredictionUsingMean(movieID, neighbourList, userVectors):
+	sumOfRatings = 0
+	k = len(neighbourList)
+	
+	if len(neighbourList) != 0:
+		weight = 1.0/len(neighbourList)
+		
+	for neighbour in neighbourList:
+		user = userVectors.getrow(neighbour[0])
+		movie = user.getcol(movieID)
+		if movie.nnz != 0:
+			sumOfRatings += (weight * movie.data[0])
+	print sumOfRatings
+	return sumOfRatings + imputationConstant
+
+def computePredictionUsingWeightedMean(movieID, neighbourList, userVectors):
+	sumOfRatings = 0
+	runningSum = 0
+	
+	for neighbour in neighbourList:
+		similarity = neighbour[1]
+		runningSum += similarity
+		user = userVectors.getrow(neighbour[0])
+		movie = user.getcol(movieID)
+		
+		if movie.nnz != 0:
+			sumOfRatings += (similarity * movie.data[0])
+	
+	if runningSum != 0:
+		sumOfRatings /= runningSum
+	print sumOfRatings
+	return sumOfRatings + imputationConstant
+
+def predictRatingsUsingUserSimilarity(useDotProduct=True, useWeightedMean=True):
+	[numUsers, numMovies, userVectors] = getUserVectors()
+	ratingsFile = open("ratings.txt", "w")
+	f = open("HW4_data/dev.csv")
+	
+	beforeTime = time.time()
+	for line in iter(f):
+		movieID = int(line.split(",")[0])
+		userID = int(line.split(",")[1])
+		print "Computing Prediction for user-movie: " + str(userID) + "-" + str(movieID)
+		neighbourList = findKSimilarUsers(10, userID, userVectors, numUsers, useDotProduct)
+		
+		rating = 0
+		if useWeightedMean is True:
+			rating = computePredictionUsingWeightedMean(movieID, neighbourList, userVectors)
+		else:
+			rating = computePredictionUsingMean(movieID, neighbourList, userVectors)
+		ratingsFile.write(str(rating) + "\n")
+	f.close()
+	ratingsFile.close()
+	afterTime = time.time()
+	print "Time Taken for online Computation: " + str(afterTime - beforeTime)
+
+def getMeanRating(userID, movieID, neighbourList, itemAssociations, userVectors):
+	sumOfRatings = 0
+	runningSum = 0
+	user = userVectors.getrow(userID)
+	
+	if len(neighbourList) != 0:
+		weight = 1.0/len(neighbourList)
+
+	for neighbour in neighbourList:
+		movie = user.getcol(neighbour[0])
+		if movie.nnz != 0:
+			sumOfRatings += (weight * movie.data[0])
+	print sumOfRatings
+	return sumOfRatings + imputationConstant
+	
+def getWeighedMeanRating(userID, movieID, neighbourList, itemAssociations, userVectors):
+	sumOfRatings = 0
+	runningSum = 0
+	user = userVectors.getrow(userID)
+	
+	for neighbour in neighbourList:
+		movie = user.getcol(neighbour[0])
+		weight = itemAssociations.getrow(movieID).getcol(neighbour[0])
+		
+		if weight.nnz != 0:
+			runningSum += weight.data[0]
+			if movie.nnz != 0:
+				sumOfRatings += (weight.data[0] * movie.data[0])
+				
+	if runningSum != 0:
+		sumOfRatings /= (runningSum + 0.0)
+	print sumOfRatings
+	return sumOfRatings + imputationConstant
+
+def predictRatingsUsingMovieSimilarity(useDotProduct=True, useWeightedMean=True):
+	[numUsers, numMovies, userVectors] = getUserVectors()
+	itemAssociations = []
+	if useDotProduct is True:
+		itemAssociations = computeItemAssociationsUsingDotProduct(userVectors)
+	else:
+		itemAssociations = computeItemAssociationsUsingCosineSimilarity(userVectors, numMovies)
+
+	ratingsFile = open("ratings.txt", "w")
+	f = open("HW4_data/dev.csv")
+
+	beforeTime = time.time()
+	for line in iter(f):
+		movieID = int(line.split(",")[0])
+		userID = int(line.split(",")[1])
+		print "Computing Prediction for user-movie: " + str(userID) + "-" + str(movieID)
+		neighbourList = findKSimilarMovies(500, movieID, itemAssociations, numMovies)
+		rating = 0
+		if useWeightedMean is True:
+			rating = getWeighedMeanRating(userID, movieID, neighbourList, itemAssociations, userVectors)
+		else:
+			rating = getMeanRating(userID, movieID, neighbourList, itemAssociations, userVectors)
+		ratingsFile.write(str(rating) + "\n")
+	f.close()
+	ratingsFile.close()
+	afterTime = time.time()
+	print "Time Taken for online Computation: " + str(afterTime - beforeTime)
---
+++ statistics.py
@@ -0,0 +1,116 @@
+from scipy.sparse import csr_matrix
+
+f = open("HW4_data/train.csv")
+numUsers = 0
+numMovies = 0
+sumOfRatings = 0
+numRatings = 0
+num1Rating = 0
+num2Rating = 0
+num3Rating = 0
+num4Rating = 0
+num5Rating = 0
+
+numMoviesRatedByUser = 0
+sumOfRatingsByUser = 0
+num1RatingByUser = 0
+num2RatingByUser = 0
+num3RatingByUser = 0
+num4RatingByUser = 0
+num5RatingByUser = 0
+
+numUsersRatedMovie = 0
+sumOfRatingsByMovie = 0
+num1RatingByMovie = 0
+num2RatingByMovie = 0
+num3RatingByMovie = 0
+num4RatingByMovie = 0
+num5RatingByMovie = 0
+
+row = []
+col = []
+data = []
+
+for line in iter(f):
+	element = line.split(",")
+	movieID = int(element[0])
+	userID = int(element[1])
+	rating = int(element[2])
+	
+	row.append(userID)
+	col.append(movieID)
+	data.append(rating)
+	
+	if rating == 1:
+		num1Rating += 1
+		if userID == 4321:
+			num1RatingByUser += 1
+		if movieID == 3:
+			num1RatingByMovie += 1
+	elif rating == 2:
+		num2Rating += 1
+		if userID == 4321:
+			num2RatingByUser += 1
+		if movieID == 3:
+			num2RatingByMovie += 1
+	elif rating == 3:
+		num3Rating += 1
+		if userID == 4321:
+			num3RatingByUser += 1
+		if movieID == 3:
+			num3RatingByMovie += 1
+	elif rating == 4:
+		num4Rating += 1
+		if userID == 4321:
+			num4RatingByUser += 1
+		if movieID == 3:
+			num4RatingByMovie += 1
+	elif rating == 5:
+		num5Rating += 1
+		if userID == 4321:
+			num5RatingByUser += 1
+		if movieID == 3:
+			num5RatingByMovie += 1
+	
+	if userID == 4321:
+		numMoviesRatedByUser += 1
+		sumOfRatingsByUser += rating
+	if movieID == 3:
+		numUsersRatedMovie += 1
+		sumOfRatingsByMovie += rating
+	
+	sumOfRatings += rating
+	numRatings += 1
+	numUsers = max(userID, numUsers)
+	numMovies = max(movieID, numMovies)
+f.close()
+numUsers += 1
+numMovies += 1
+userVectors = csr_matrix((data, (row, col)), shape=(numUsers, numMovies))
+
+print "Num Users: " + str(numUsers)
+print "Num Movies: " + str(numMovies)
+print "Avg Rating: " + str((sumOfRatings+0.0)/numRatings)
+print "Number of Ratings 1: " + str(num1Rating)
+print "Number of Ratings 2: " + str(num2Rating)
+print "Number of Ratings 3: " + str(num3Rating)
+print "Number of Ratings 4: " + str(num4Rating)
+print "Number of Ratings 5: " + str(num5Rating)
+
+print "\nFor User 4321:"
+print "Number of Movies Rated: " + str(numMoviesRatedByUser)
+print "Avg Rating: " + str((sumOfRatingsByUser+0.0)/numMoviesRatedByUser)
+print "Number of Ratings 1: " + str(num1RatingByUser)
+print "Number of Ratings 2: " + str(num2RatingByUser)
+print "Number of Ratings 3: " + str(num3RatingByUser)
+print "Number of Ratings 4: " + str(num4RatingByUser)
+print "Number of Ratings 5: " + str(num5RatingByUser)
+
+print "\nFor Movie 3:"
+print "Number of Users Rated the Movie: " + str(numUsersRatedMovie)
+print "Avg Rating: " + str((sumOfRatingsByMovie+0.0)/numUsersRatedMovie)
+print "Number of Ratings 1: " + str(num1RatingByMovie)
+print "Number of Ratings 2: " + str(num2RatingByMovie)
+print "Number of Ratings 3: " + str(num3RatingByMovie)
+print "Number of Ratings 4: " + str(num4RatingByMovie)
+print "Number of Ratings 5: " + str(num5RatingByMovie)
